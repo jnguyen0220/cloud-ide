ceaf1b641fd3d97ce9f92f6361eb4afa8a6ceb42
Jonny Nguyen <jonny_nguyen@outlook.com>
2023-08-08 16:31:10 +0000

    remove password from cs and jl

--- script/init.sh
+++ script/init.sh
@@ -1,3 +1,3 @@
-nohup code-server --host 0.0.0.0 --port 8080 &
-nohup jupyter lab --LabApp.base_url=jl --ip 0.0.0.0 --allow-root --LabApp.token=${PASSWORD} &
+nohup code-server --host 0.0.0.0 --auth none --port 8080 &
+nohup jupyter lab --LabApp.base_url=jl --ip 0.0.0.0 --allow-root --LabApp.token="" --LabApp.password="" &
 nginx -g 'daemon off;'
