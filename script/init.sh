c9656292027677be978e7ae226324ec98784f54f
Jonny Nguyen <jonny_nguyen@outlook.com>
2023-08-25 01:35:07 +0000

    update not to start web browser

--- script/init.sh
+++ script/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 nohup code-server --host 0.0.0.0 --auth none --port 8080 &
-nohup jupyter lab --LabApp.base_url=jl --ip 0.0.0.0 --allow-root --LabApp.token="" --LabApp.password="" &
+nohup jupyter lab --LabApp.base_url=jl --ip 0.0.0.0 --allow-root --LabApp.token="" --LabApp.password="" --LabApp.open_browser="False" &
 sudo nginx
 wait -n
